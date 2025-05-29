1c7fcc43145c70809f7f52f061717b367e84225a
Cyprian <lightningcyranus@gmail.com>
2025-05-29 07:19:09 +0200

    created employee object

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+// Define an employee object
+let employee = {
+  name: "John Doe",
+  streetAddress: "123 Main St",
+}
+
+//updateEmployee With Key Value
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+//mutates the original employee object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {	
+  delete employee[key];
+  return employee;
+}
