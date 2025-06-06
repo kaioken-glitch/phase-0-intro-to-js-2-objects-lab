56553337c1e38058d5f035dc55cb15c1729db6dc
Cyprian <lightningcyranus@gmail.com>
2025-06-06 12:45:19 +0200

    created employee object and modified and deleted

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
   employee[key] = value;
   return employee;
 }
+
 function deleteFromEmployeeByKey(employee, key) {
   const newEmployee = { ...employee };
   delete newEmployee[key];
   return newEmployee;
 }
+
 function destructivelyDeleteFromEmployeeByKey(employee, key) {	
   delete employee[key];
   return employee;
